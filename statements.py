a96aaa6bd005df9f103662cc304e2a8a78ec98e4
Liloh Kimanzi <kimanzililian6@gmail.com>
2023-11-22 11:26:34 +0300

    update

--- statements.py
+++ statements.py
@@ -1,43 +1,43 @@
-# marks=30
-# if(marks>=60):
-#     print("A")
-# elif(marks>=50):
-#     print("B")
-# elif(marks>=40):
-#     print("C")
-# else:
-#     print("fail")
+marks=30
+if(marks>=60):
+    print("A")
+elif(marks>=50):
+    print("B")
+elif(marks>=40):
+    print("C")
+else:
+    print("fail")
 
-#     average_marks=int(input("enter marks:"))
-#     if(average_marks>=70):
-#         print("A")
-#     elif(average_marks>=60 and average_marks<70):
-#         print("B")
-#     elif(average_marks>=50 and average_marks<60):
-#         print("C")
-#     elif(average_marks>=40 and average_marks<50):
-#         print("D")
-#     else:
-#         print("E")
+    average_marks=int(input("enter marks:"))
+    if(average_marks>=70):
+        print("A")
+    elif(average_marks>=60 and average_marks<70):
+        print("B")
+    elif(average_marks>=50 and average_marks<60):
+        print("C")
+    elif(average_marks>=40 and average_marks<50):
+        print("D")
+    else:
+        print("E")
 
 # # Take three inputs from a user, separately. Print the largest of the numbers.
 # #     Hint: Determine what type of data is taken in as input
 
-input1=int(input("enter input1:"))
-input2=int(input("enter input2:"))
-input3=int(input("enter input3:"))
-if(input1>input2 and input1>input3):
-            print("f{input1} is the largest")
-elif(input2>input1 and input2>input3):
-            print("f{input2} is the largest")
-else:
-           print("f{input3} is the largest")
-
-
-# Take as input from a user the temperature if the temperature is above 30°C display “The temperature is too high”, otherwise display “Normal temperature”
-
-# temp=int(input("enter temp:"))
-# if(temp>30):
-#       print("the temperature is too high")
+# input1=int(input("enter input1:"))
+# input2=int(input("enter input2:"))
+# input3=int(input("enter input3:"))
+# if(input1>input2 and input1>input3):
+#             print("f{input1} is the largest")
+# elif(input2>input1 and input2>input3):
+#             print("f{input2} is the largest")
 # else:
-#       print("normal temperature")
+#            print("f{input3} is the largest")
+
+
+# # Take as input from a user the temperature if the temperature is above 30°C display “The temperature is too high”, otherwise display “Normal temperature”
+
+# # temp=int(input("enter temp:"))
+# # if(temp>30):
+# #       print("the temperature is too high")
+# # else:
+# #       print("normal temperature")
