66425c3a23584c8b21aa8fd5dbe5e1ae2e48371e
Liloh Kimanzi <kimanzililian6@gmail.com>
2023-11-22 11:19:07 +0300

    update

--- listtask.py
+++ listtask.py
@@ -6,3 +6,18 @@ print(trainees[1][0])
 print(trainees[1][1][0])
 
 #  Using a method add 56 at the end of the list.
+trainees.append(56)
+print(trainees)
+
+# Using a method add the name Mike between James and Mary
+trainees.insert(2,"mike")
+print(trainees)
+
+# Change the value of 2 to 8
+trainees=[1][0]=8
+print(trainees)
+
+# Remove John and Mary from the list.
+trainees = ["John", [2, ["James","Mary"]]]
+trainees.remove()
+
--- operators.py
+++ operators.py
@@ -8,3 +8,9 @@ num2=4
 print(num2**2)
 # modulus
 print(100%11)
+
+# logical operators
+# assignment operators
+x=3
+x=x+5
+print(x)
